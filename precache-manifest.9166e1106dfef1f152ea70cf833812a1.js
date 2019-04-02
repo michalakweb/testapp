defadb578513290fb614db717f055df56dcbe74c
Mateusz Michalak <michalakweb@gmail.com>
2019-04-02 21:13:20 +0200

    Updates

---
+++ precache-manifest.9166e1106dfef1f152ea70cf833812a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b246a085b16065961aeb",
+    "url": "/MyApp/static/css/main.e12b7be4.chunk.css"
+  },
+  {
+    "revision": "b246a085b16065961aeb",
+    "url": "/MyApp/static/js/main.75dfafec.chunk.js"
+  },
+  {
+    "revision": "663bb388ea8306a9f179",
+    "url": "/MyApp/static/js/runtime~main.0c70bd8d.js"
+  },
+  {
+    "revision": "d4242c34e53cf35570c7",
+    "url": "/MyApp/static/css/2.367ddddf.chunk.css"
+  },
+  {
+    "revision": "d4242c34e53cf35570c7",
+    "url": "/MyApp/static/js/2.c2cb62ed.chunk.js"
+  },
+  {
+    "revision": "753478813d36607afa2fa509720d333c",
+    "url": "/MyApp/index.html"
+  }
+];
